1c21c5d6850db644133a054f6df3634455801318
ShivaAbf <88429496+ShivaAbf@users.noreply.github.com>
2022-04-12 01:38:13 +0430

    html, css and js updated

--- src/app.js
+++ src/app.js
@@ -1,48 +1,64 @@
 let dates = document.querySelector("#date");
 let time = document.querySelector("#time");
-let currentDate = new Date();
-// console.log(currentDate);
-let weekDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let week = weekDay[currentDate.getDay()];
-let mounth = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "Auguast",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let mounths = mounth[currentDate.getMonth()];
-// console.log(week);
-let day = currentDate.getDate();
-dates.innerHTML = `${week},  ${mounths}  ${day}`;
-// time.innerHTML = `${currentDate.getHours()}:${currentDate.getMinutes()}`;
-let hour = currentDate.getHours();
-let min = currentDate.getMinutes();
-if(hour <10){
-hour = `0${hour}`
+function showTime(currentTime){
+  let currentDate = new Date();
+  // console.log(currentDate);
+  let weekDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let week = weekDay[currentDate.getDay()];
+  let mounth = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Auguast",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let mounths = mounth[currentDate.getMonth()];
+  // console.log(week);
+  let day = currentDate.getDate();
+  dates.innerHTML = `${week},  ${mounths}  ${day}`;
+  // time.innerHTML = `${currentDate.getHours()}:${currentDate.getMinutes()}`;
+  let hour = currentDate.getHours();
+  let min = currentDate.getMinutes();
+  if(hour <10){
+  hour = `0${hour}`
+  }
+  if(min<10){
+    min = `0${min}`
+  }
+  time.innerHTML = `${hour}:${min}`
 }
-if(min<10){
-  min = `0${min}`
-}
-time.innerHTML = `${hour}:${min}`
+
+showTime()
 let form = document.querySelector("form");
 let input = document.querySelector("#enter-city");
+let icon = document.querySelector("#icon")
 
 
 
 // console.log(form.value);
 function showWeather(information){
-  let weath = document.querySelector("#weath")
-  let weather = Math.round(information.data.main.temp);
-  weath.innerHTML = `${weather}°C`
 
+  let weath = document.querySelector("#weath")
+  
+  weath.innerHTML =  Math.round(information.data.main.temp);
+  let description = document.querySelector("#desc")
+   description.innerHTML= information.data.weather[0].main;
+  let humidity = document.querySelector("#humid");
+humidity.innerHTML = `Humidity: ${information.data.main.humidity}%`
+let wind = document.querySelector("#wind")
+wind.innerHTML = `Wind speed: ${Math.round(information.data.wind.speed)} Km/H`
+icon.setAttribute(
+  "src",
+  `http://openweathermap.org/img/wn/${information.data.weather[0].icon}@2x.png`
+);
+  
 
 }
 function search(city){
@@ -66,4 +82,5 @@ function search(city){
 
 
   form.addEventListener("submit", showCity);
+   let fahrenheit = document.querySelector("#fahrenheit")
   
