a22a2ccf6df815e3feac1fc1e1063b73a7475942
ShivaAbf <88429496+ShivaAbf@users.noreply.github.com>
2022-04-30 00:49:39 +0430

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,24 +5,10 @@ function showTime(currentTime){
   let weekDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let week = weekDay[currentDate.getDay()];
   let mounth = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "Auguast",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+    "January","February","March","April","May","June","July","Auguast","September","October","November","December",];
   let mounths = mounth[currentDate.getMonth()];
-  // console.log(week);
   let day = currentDate.getDate();
   dates.innerHTML = `${week},  ${mounths}  ${day}`;
-  // time.innerHTML = `${currentDate.getHours()}:${currentDate.getMinutes()}`;
   let hour = currentDate.getHours();
   let min = currentDate.getMinutes();
   if(hour <10){
@@ -41,80 +27,78 @@ let icon = document.querySelector("#icon")
 
 function getCoordinates(Coordinate){
   apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${Coordinate.lat}&lon=${Coordinate.lon}&appid=8269c637bf5ee65986b1b74910708602&units=metric`
-  console.log(apiUrl)
   axios.get(apiUrl).then(showForecast)
 }
 
-
-// console.log(form.value);
 function showWeather(information){
-
-  let weath = document.querySelector("#weath")
-  celsiusTemp = information.data.main.temp
-  console.log(celsiusTemp)
-  weath.innerHTML =  Math.round(celsiusTemp);
-  let description = document.querySelector("#desc")
-   description.innerHTML= information.data.weather[0].main;
-  let humidity = document.querySelector("#humid");
-humidity.innerHTML = `Humidity: ${information.data.main.humidity}%`
+let weath = document.querySelector("#weath")
+let description = document.querySelector("#desc")
+let humidity = document.querySelector("#humid");
 let wind = document.querySelector("#wind")
+
+celsiusTemp = information.data.main.temp
+weath.innerHTML =  Math.round(celsiusTemp);
+description.innerHTML= information.data.weather[0].main;
+humidity.innerHTML = `Humidity: ${information.data.main.humidity}%`
 wind.innerHTML = `Wind speed: ${Math.round(information.data.wind.speed)} Km/H`
 icon.setAttribute(
   "src",
   `http://openweathermap.org/img/wn/${information.data.weather[0].icon}@2x.png`
 );
-console.log(information)
 getCoordinates(information.data.coord)
-
 }
+
 function search(city){
- 
-  apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8269c637bf5ee65986b1b74910708602&units=metric`
-  axios.get(apiUrl).then(showWeather);
-  // console.log(city)
+apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8269c637bf5ee65986b1b74910708602&units=metric`
+axios.get(apiUrl).then(showWeather);
 }
 
-
-
- 
-  function showCity(cityValue) {
-    cityValue = document.querySelector("#show-city");
-    event.preventDefault();
-    let celsiusTemp = null;
-    cityValue.innerHTML = input.value;
-    search(input.value);
+function showCity(cityValue) {
+  event.preventDefault();
+  cityValue = document.querySelector("#show-city");
+  let celsiusTemp = null;
+  cityValue.innerHTML = input.value;
+  search(input.value);
   }
+
 function showFahrenheit(event){
   event.preventDefault();
   let fahrenheitTemp = (celsiusTemp * 9/5) + 32
   let temperature = document.querySelector("#weath")
   temperature.innerHTML = Math.round(fahrenheitTemp) 
-  
-}
+  }
+
 function showCelcius(event){
   event.preventDefault();
   let temperature =document.querySelector("#weath")
   temperature.innerHTML = Math.round(celsiusTemp)
 }
+function formatDay(timeNumber, index){
+  console.log(index)
+let date = new Date(timeNumber * 1000);
+let day = date.getDay();
+let weekDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+return weekDay[day]
+}
 
 function showForecast(response){
-  console.log(response.data.daily)
+
   let forecastElement = document.querySelector("#forecast")
-  let weekDay = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let dailyForecast = response.data.daily;
+  console.log(dailyForecast)
   let forecastHTML = `<div class="row">`
-  weekDay.forEach(function(day){
-    
+  dailyForecast.forEach(function(responseDay,index ){
+    if(index < 6){
     forecastHTML =forecastHTML + `
     <div class="col-2 week ">
         <ul>
-        <li><p class="week-day">${day}</p></li>
-        <li class="weather my-2"><img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"></li>
-        <li><span class="min-temp">15°</span>   <span class="max-temp">18°</span></li>
+        <li><p class="week-day">${formatDay(responseDay.dt)}</p></li>
+        <li class="weather my-2"><img src="http://openweathermap.org/img/wn/${responseDay.weather[0].icon}@2x.png"></li>
+        <li><span class="min-temp">${Math.round(responseDay.temp.min)}°</span>   <span class="max-temp">${Math.round(responseDay.temp.max)}°</span></li>
       </ul>
-      </div>
-  `
-  
-    forecastElement.innerHTML = forecastHTML
+      </div>`
+  forecastElement.innerHTML = forecastHTML
+}
   })
   forecastHTML = forecastHTML + `</div>`
 }
